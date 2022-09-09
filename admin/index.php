2fa09b9468a195ab262b8958f834f86806b597d3
Havva Zeynep Akdemir <83979496+Havvazeynep@users.noreply.github.com>
2022-09-10 00:03:55 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -13,3 +13,20 @@ if($result!=false){
     echo $result->title."</br>";
 }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sıfırdan Web Tasarım Admin Panel Projesi - Havva Zeynep Akdemir">
+	<meta name="author" content="https://github.com/Havvazeynep , havvazeynepakdemir@gmail.com">
+	<meta name="keywords" content="HTML CSS JS Javascript Web Tasarım SQL mysql PHP">
+    <title>Admin Deneme</title>
+</head>
+<body>
+    
+</body>
+</html>
